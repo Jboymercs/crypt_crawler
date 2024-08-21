244569cf7f0d617e2a2260796d7be11a784fea27
Un-Original1 <78579980+Un-Original1@users.noreply.github.com>
2024-08-21 14:09:28 -0500

    Updated Mod Id
    Let's try not to use 2 letters only, it is meant to be unique, to prevent conflicts.

--- src/main/java/com/unseen/cc/util/ModReference.java
+++ src/main/java/com/unseen/cc/util/ModReference.java
@@ -1,7 +1,7 @@
 package com.unseen.cc.util;
 
 public class ModReference {
-    public static final String MOD_ID ="cc";
+    public static final String MOD_ID ="cryptcrawler";
     public static final String NAME = "Crypt Crawler";
     public static final String CHANNEL_NETWORK_NAME = "Crypt Crawler";
 
